49e9aa42d1d6f215e381749bf4ddf839792530fc
duynghiadev <duynghia22302@gmail.com>
2023-08-25 15:32:51 +0700

    #20. Design App Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.scss";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import TableUsers from "./components/TableUsers";
 import Container from "react-bootstrap/Container";
 import { ToastContainer } from "react-toastify";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
       <div className="app-container">
         <Header />
         <Container>
-          <TableUsers />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<TableUsers />} />
+          </Routes>
         </Container>
       </div>
       <ToastContainer
